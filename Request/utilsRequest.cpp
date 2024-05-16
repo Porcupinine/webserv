dc940b65f6948ddc0965569724e18a70e0a8cb79
Dominique Maessen <dmaessen@f0r2s14.codam.nl>
2024-05-16 11:45:27 +0200

    Request folder added

---
+++ Request/utilsRequest.cpp
@@ -0,0 +1,29 @@
+#include "../Request/parseRequest.hpp"
+
+std::string& parseRequest::capsOn(std::string &str) {
+    for (size_t i = 0; i < str.size(); i++) {
+        if (str[0])
+            str[0] = toupper(str[0]);
+        if (str[i - 1] == '-' && isalpha(str[i])) // this correct or !=
+            str[i] = toupper(str[i]);
+    }
+    return str;
+}
+
+std::string parseRequest::rmSpaces(std::string &str) {
+    size_t len;
+    len = str.size() - 1;
+    while (len >= 0 && str[len] == ' ')
+        --len;
+    return str.substr(0, len + 1); // to test
+}
+
+std::vector<std::string> parseRequest::split(const std::string &str, char c) {
+    std::vector<std::string> vec;
+    std::string element;
+    std::istringstream stream(str);
+
+    while (std::getline(stream, element, c))
+        vec.push_back(element);
+    return vec;
+}
