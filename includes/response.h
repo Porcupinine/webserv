b45d85846ba7d1959c3fc84e024871ba7d881b86
Laura <eccentricporcupine@gmail.com>
2024-05-27 11:07:57 +0200

    can I change branches please?

---
+++ includes/response.h
@@ -0,0 +1,17 @@
+//
+// Created by laura on 16-5-24.
+//
+
+#ifndef WEBSERV_RESPONSE_H
+#define WEBSERV_RESPONSE_H
+
+
+
+class response {
+private:
+	std::string	m_response{};
+	size_t
+};
+
+
+#endif //WEBSERV_RESPONSE_H
--- src/cgiHandler.cpp
+++ src/cgiHandler.cpp
@@ -2,5 +2,5 @@
 // Created by laura on 21-4-24.
 //
 
-#include "CGI.h"
+#include "cgiHandler.h"
 
