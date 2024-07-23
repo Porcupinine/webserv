518d893c5d8f4e5341cf130a9d7fb09c27495182
Esiwehl <59033264+Esiwehl@users.noreply.github.com>
2024-07-23 12:13:08 +0200

    Removed comments

--- inc/VirtualHost.hpp
+++ inc/VirtualHost.hpp
@@ -1,31 +1,3 @@
-// #pragma once
-// #ifndef VIRTUAL_HOST
-// # define VIRTUAL_HOST
-
-// # include <sys/socket.h> // For socket functions
-// # include <netinet/in.h> // For sockaddr_in
-// # include <cstdlib> // For exit() and EXIT_FAILURE
-// # include <iostream> // For cout
-// # include <unistd.h> // For read
-
-// # include "defines.hpp"
-
-// class VirtualHost {
-// 	private:
-// 		uint16_t								_port;
-
-// 		int										_listenSocket;
-// 		struct sockaddr_in						_address;
-
-// 		struct ServerConfig						_settings;
-// 		std::unique_ptr<struct SharedData>		*_shared; // is this how this works?
-
-// 	public:
-
-// };
-
-// #endif
-
 #pragma once
 
 #include "defines.hpp"
