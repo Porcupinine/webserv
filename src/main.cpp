c84ebdb01c7a63de1ea0e84b20777eed87a33b2b
Eluse Wehl <ewehl@f0r3s22.codam.nl>
2024-08-04 20:12:46 +0200

    slightly better errorhandling

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char **argv){
 		WebServ server(argc, argv);
 		server.run();
 	} catch (Config::ParseException &e) {
-		std::cout << "Fuck " << e.what() << std::endl;
+		std::cout << "Parse exception: " << e.what() << std::endl;
+	} catch (WebServ::InitException &e) {
+		std::cout << "InitException: " << e.what() << std::endl;
 	} catch (std::exception &e) {
 		std::cout << "Fuck#2 " << e.what() << std::endl;
 	}
